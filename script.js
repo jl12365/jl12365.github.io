80ec588334b8d4e7ff7840299afc9fe413bf7271
Jason <jasonli12365@gmail.com>
2024-05-06 23:23:16 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const Menu = document.querySelector(".menu-links");
+    const Icon = document.querySelector(".ham-icon");
+    Menu.classList.toggle("open")
+    Icon.classList.toggle("open")
+}
